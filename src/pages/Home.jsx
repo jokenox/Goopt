cf6101d650f5220aab12b7a752857744f8445b0a
jokenox <jokenox@gmail.com>
2021-12-10 12:03:14 -0600

    Set up TailwindCSS

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import '../index.css';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className="bg-indigo-500">
+        Goopt
+      </div>
+    )
+  }
+}
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {},
   },
