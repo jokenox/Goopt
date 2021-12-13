682f1f86f9ad3a07ae4348d7be5d371c8fa6fc3e
jokenox <jokenox@gmail.com>
2021-12-13 14:38:00 -0600

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import { AppContextProvider } from './contexts/AppContext/AppContext';
+
 import Home from './pages/Home';
+import Results from './pages/Results';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <AppContextProvider>
+          <Routes>
+            <Route path="/results" element={ <Results /> } />
+            <Route exact path="/" element={ <Home /> } />
+          </Routes>
+        </AppContextProvider>
+      </BrowserRouter>
     </div>
   );
 }
