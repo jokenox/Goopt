0137d155dadc30e1f2fd0b6d6b1d0b07a073b3f8
jokenox <jokenox@gmail.com>
2021-12-13 14:39:29 -0600

    Add AppContext

---
+++ src/contexts/AppContext/AppContext.jsx
@@ -0,0 +1,32 @@
+import React, { createContext, useState, useRef } from 'react';
+import { useNavigate } from "react-router-dom";
+
+const AppContext = createContext();
+
+const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+
+  const [searchTerm, setSearchTerm] = useState();
+
+  const gooptSearch = () => {
+    navigate('/results?search=' + searchTerm);
+  };
+
+  const imFeelingLucky = () => {
+    navigate('/site?search=' + searchTerm);
+  };
+
+  return (
+    <AppContext.Provider value={{
+      setSearchTerm,
+      gooptSearch,
+      imFeelingLucky
+    }}>
+      { children }
+    </AppContext.Provider>
+  );
+
+
+};
+
+export { AppContext, AppContextProvider };
