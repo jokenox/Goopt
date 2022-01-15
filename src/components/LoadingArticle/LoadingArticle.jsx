a29314aaf415539601a214897c9f49c4adb57166
jokenox <jokenox@gmail.com>
2022-01-15 05:09:08 -0600

    Add LoadingArticle component

---
+++ src/components/LoadingArticle/LoadingArticle.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function LoadingArticle() {
+  return (
+    <div className="w-full animate-pulse">
+      <div className="w-full h-5 mb-3 bg-googray-light"></div>
+      <div className="w-full h-5 mb-3 bg-googray-light"></div>
+      <div className="w-full h-5 mb-3 bg-googray-light"></div>
+      <div className="w-full h-5 mb-3 bg-googray-light"></div>
+      <div className="w-full h-5 mb-3 bg-googray-light"></div>
+      <div className="w-full h-5 mb-3 bg-googray-light"></div>
+      <div className="w-full h-5 mb-3 bg-googray-light"></div>
+      <div className="w-full h-5 mb-3 bg-googray-light"></div>
+      <div className="w-full h-5 mb-3 bg-googray-light"></div>
+      <div className="w-full h-5 mb-3 bg-googray-light"></div>
+      <div className="w-full h-5 mb-3 bg-googray-light"></div>
+      <div className="w-full h-5 mb-3 bg-googray-light"></div>
+      <div className="w-full h-5 bg-googray-light"></div>
+    </div>
+  );
+}
+
+export default LoadingArticle;
