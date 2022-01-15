cee9ce4c565e24ea8babfa8864506603eacfa410
jokenox <jokenox@gmail.com>
2022-01-15 04:23:25 -0600

    Add LoadingResult component

---
+++ src/components/LoadingResult/LoadingResult.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function LoadingResult() {
+  return (
+    <div className="w-6/12 animate-pulse">
+      <div className="w-full h-8 mb-3 bg-googray-light"></div>
+
+      <div className="w-full h-4 mb-3 bg-googray-light"></div>
+      <div className="w-full h-4 mb-3 bg-googray-light"></div>
+      <div className="w-full h-4 mb-3 bg-googray-light"></div>
+      <div className="w-full h-4 mb-6 bg-googray-light"></div>
+
+
+      <div className="w-full h-8 mb-3 bg-googray-light"></div>
+
+      <div className="w-full h-4 mb-3 bg-googray-light"></div>
+      <div className="w-full h-4 mb-3 bg-googray-light"></div>
+      <div className="w-full h-4 mb-3 bg-googray-light"></div>
+      <div className="w-full h-4 mb-6 bg-googray-light"></div>
+    </div>
+  );
+}
+
+export default LoadingResult;
