73e2b88171be32061eb4a6bbd1fd72ab5915c171
jokenox <jokenox@gmail.com>
2022-01-15 04:10:04 -0600

    Add AppContext

--- src/contexts/AppContext/AppContext.jsx
+++ src/contexts/AppContext/AppContext.jsx
@@ -1,27 +1,158 @@
-import React, { createContext, useState, useRef } from 'react';
+import React, { createContext, useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import OpenAI from 'openai-api';
+
+import formatResults from '../../utils/formatResults';
+import { SearchTemplate, ArticleTemplate } from '../../utils/templates';
 
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
   const navigate = useNavigate();
+  const openai = new OpenAI(process.env.REACT_APP_OPENAI_API_KEY);
 
-  const [searchTerm, setSearchTerm] = useState();
+  const [language, setLanguage] = useState((localStorage.getItem('language') || 'en')); 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoadingResults, setIsLoadingResults] = useState(false);
+  const [isLoadingArticle, setIsLoadingArticle] = useState(false);
 
-  const gooptSearch = () => {
-    navigate('/results?search=' + searchTerm);
+  const selectLanguage = (lang) => {
+    localStorage.setItem('language', lang);
+    setLanguage(lang);
   };
 
-  const imFeelingLucky = () => {
-    navigate('/site?search=' + searchTerm);
+  const generateArticleText = async (text) => {
+    setIsLoadingArticle(true);
+
+    const gptResponse = await openai.complete({
+      engine: 'davinci-instruct-beta-v3',
+      prompt: text.trim(),
+      maxTokens: 500,
+      temperature: 0.7,
+      topP: 1,
+      presencePenalty: 0,
+      frequencyPenalty: 2,
+      bestOf: 1,
+      n: 1,
+      stream: false
+    });
+
+    console.log(gptResponse.data.choices[0].text);
+
+    setIsLoadingArticle(false);
+
+    return gptResponse.data.choices[0].text;
+  };
+
+  const generateResults = async (searchTerm) => {
+    const query = getSearchTemplate() + searchTerm + '\n\n{';
+
+    setIsLoadingResults(true);
+
+    const gptResponse = await openai.complete({
+      engine: 'davinci-instruct-beta-v3',
+      prompt: query,
+      maxTokens: 1000,
+      temperature: 1,
+      topP: 1,
+      presencePenalty: 0,
+      frequencyPenalty: 2,
+      bestOf: 1,
+      n: 1,
+      stream: false,
+      stop: ['"""']
+    });
+
+    console.log(gptResponse.data.choices[0].text);
+
+    setIsLoadingResults(false);
+
+    return gptResponse.data.choices[0].text;
+  };
+
+  const getResults = async (term) => {
+    setSearchResults([]);
+
+    let results = formatResults(await generateResults(term));
+    let numberOfResults = results.length;
+
+    setSearchResults(results);
+
+    if (numberOfResults < 3) getMoreResults(numberOfResults);
+  };
+
+  const getMoreResults = async (numberOfPrevResults) => {
+    let newResults = formatResults(await generateResults(searchTerm));
+    let numberOfResults = numberOfPrevResults + newResults.length;
+
+    setSearchResults(prev => [...prev, ...newResults]);
+
+    if (numberOfResults < 3) getMoreResults(numberOfResults);
+  };
+
+  const gooptSearch = (term) => {
+    if (term.trim().length > 0) {
+      navigate('/results?search=' + term.trim());
+    
+      getResults(term.trim());
+    }
+  };
+
+  const imFeelingLucky = async (term) => {
+    if (searchTerm.trim().length > 0) {
+      navigate('/site?term=' + term.trim());
+
+      await getResults(term.trim());
+    }
+  };
+
+  const getSearchTemplate = (lang = language) => {
+    switch (lang) {
+      case 'en':
+        return SearchTemplate.en;
+      
+      case 'es':
+        return SearchTemplate.es;
+
+      case 'eo':
+        return SearchTemplate.eo;
+
+      default:
+        return false;
+    };
+  };
+
+  const getArticleTemplate = (lang = language) => {
+    switch (lang) {
+      case 'en':
+        return ArticleTemplate.en;
+      
+      case 'es':
+        return ArticleTemplate.es;
+
+      case 'eo':
+        return ArticleTemplate.eo;
+
+      default:
+        return false;
+    };
   };
 
   return (
     <AppContext.Provider value={{
+      language,
+      selectLanguage,
       searchTerm,
       setSearchTerm,
+      searchResults,
+      isLoadingResults,
+      isLoadingArticle,
+      generateArticleText,
       gooptSearch,
-      imFeelingLucky
+      getMoreResults,
+      imFeelingLucky,
+      getArticleTemplate
     }}>
       { children }
     </AppContext.Provider>
